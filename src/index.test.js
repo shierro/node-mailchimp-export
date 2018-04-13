122da82c7659044cddd6b30c836f10acb0c7b283
shierro <theodoro.pelingan@gmail.com>
2018-04-13 22:17:00 +0800

    fixed duplicates found by codeclimate

--- src/index.test.js
+++ src/index.test.js
@@ -2,7 +2,7 @@
 const MailchimpExport = require('./index');
 const { expect } = require('chai');
 
-describe('src/index.js', () => {
+describe('src/index.js - Instance', () => {
   it('should create new instance without fail', () => {
     const MAILCHIMP_TOKEN = 'testToken-us17';
     const mailchimpExport = new MailchimpExport(MAILCHIMP_TOKEN);
@@ -21,25 +21,23 @@ describe('src/index.js', () => {
       expect(mailchimpExport).to.equal(undefined);
     }
   });
-
+});
+describe('src/index.js - Usage', () => {
+  const MAILCHIMP_TOKEN = 'testToken-us17';
+  const mailchimpExport = new MailchimpExport(MAILCHIMP_TOKEN);
   it('should return a request object successfully when list is called', () => {
-    const MAILCHIMP_TOKEN = 'testToken-us17';
-    const mailchimpExport = new MailchimpExport(MAILCHIMP_TOKEN);
-    const requestObject = mailchimpExport.list({ id: 'testId' });
+    const requestObject = mailchimpExport.list({ id: 'listId' });
     expect(requestObject).to.contain.keys(['__isRequestRequest', 'httpModule', 'agent']);
   });
 
   it('should return a request object successfully when campaignSubscriberActivity is called', () => {
-    const MAILCHIMP_TOKEN = 'testToken-us17';
-    const mailchimpExport = new MailchimpExport(MAILCHIMP_TOKEN);
-    const requestObject = mailchimpExport.campaignSubscriberActivity({ id: 'testId' });
+    const requestObject = mailchimpExport.campaignSubscriberActivity({ id: 'campaignId' });
+    expect(typeof requestObject).to.equal('object');
     expect(requestObject).to.contain.keys(['__isRequestRequest', 'httpModule', 'agent']);
   });
 
   it('should throw error when validation params is not an object', () => {
     try {
-      const MAILCHIMP_TOKEN = 'testToken-us17';
-      const mailchimpExport = new MailchimpExport(MAILCHIMP_TOKEN);
       mailchimpExport.validateParams();
     } catch (e) {
       expect(e.message).to.equal('params must be an object');
@@ -48,8 +46,6 @@ describe('src/index.js', () => {
 
   it('should throw error when validation params does not have an `id`', () => {
     try {
-      const MAILCHIMP_TOKEN = 'testToken-us17';
-      const mailchimpExport = new MailchimpExport(MAILCHIMP_TOKEN);
       mailchimpExport.validateParams({ test: 'test' });
     } catch (e) {
       expect(e.message).to.equal('id not found');
